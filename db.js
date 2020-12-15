349583671ec5d2693c8a33ed34985b5ac1a6fbb9
Minh Nghiem Quoc Anh 3 (FIS TDC HCM) <minhnqa3@fpt.com.vn>
2020-12-16 01:00:16 +0700

    cmt

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); // khai báo mongoose
 
-mongoose.connect('mongodb://localhost:27017/portfoliodb', { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/portfoliodb', { useNewUrlParser: true, useUnifiedTopology: true }
     , err => {
         if (!err) {
             console.log("Connect mongodb successed!")
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');//https://topdev.vn/blog/bodyparser-trong-express-js/
 const PORT = process.env.PORT || 4000;
 const cors = require('cors');
+const path = require(path);
 // connect mongodb
 require('./db');
 
@@ -18,7 +19,12 @@ app.use(express.static('upload'))
 app.use('/category', categoryController);
 app.use('/projects', projectController);
 
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/admin/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'admin', 'build', 'index.html'))
+  })
+}
 
 
 
