e6bc56512a78d0c2c2b50f41ef27386c8e4800c9
Minh <Nghiemminh1904@gmail.com>
2021-01-01 05:17:16 +0700

    fix path

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 4000;
 const cors = require('cors');
 // connect mongodb
 require('./db');
-
+const path = require('path');
 // khai router
 const categoryController = require('./controllers/category');
 const projectController = require('./controllers/projects');
@@ -18,7 +18,12 @@ app.use(express.static('upload'))
 app.use('/category', categoryController);
 app.use('/projects', projectController);
 
+app.use(express.static(path.join(__dirname, 'build')));
 
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 
