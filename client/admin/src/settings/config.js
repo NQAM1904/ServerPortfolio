852c6e35de84da7d8d76c5816819aeabf741fcbc
Minh Nghiem Quoc Anh 3 (FIS TDC HCM) <minhnqa3@fpt.com.vn>
2021-01-03 13:38:56 +0700

    huuh

--- client/admin/src/settings/config.js
+++ client/admin/src/settings/config.js
@@ -1,4 +1,4 @@
-// const API_URL = 'https://mern-stack-admin.herokuapp.com';
-const API_URL = 'http://localhost:4000';
+const API_URL = 'https://mern-stack-admin.herokuapp.com';
+// const API_URL = 'http://localhost:4000';
 
 export default API_URL;
